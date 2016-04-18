f751f79366285782d50701d7ad30906fe60477cf
Richard Boell <Richard@dhcp657-063.laptop-wlc.uni-heidelberg.de>
2016-04-18 17:12:46 +0200

    implemented greeting UI

--- HelloWorld/AppDelegate.swift
+++ HelloWorld/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        // Dieser Code wird beim Start der App ausgeführt
         
         return true
     }
