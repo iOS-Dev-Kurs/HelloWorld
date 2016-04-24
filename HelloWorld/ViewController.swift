284feb1608180a3014f8e886984c058fcd64d70b
Arthur Heimbrecht <heimbrecht@stud.uni-heidelberg.de>
2016-04-24 18:16:15 +0200

    erstes Hello World Programm

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet var nameTextfield: UITextField!
+	@IBOutlet var greetingLabel: UILabel!
+	
+	@IBAction func greetingButtonPressed(sender: UIButton) {
+		print("Hello World!")
+		if let name = nameTextfield.text where !name.isEmpty {
+			greetingLabel.text = "Hello \(name)!"
+		} else {
+			greetingLabel.text = "Hello World!"
+		}
+	}
 
 }
-
